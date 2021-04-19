3976199bc1f39470ed2a859ce4663e2d5e8b7460
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-18 22:02:33 -0700

    reverse sub list of linked list

--- leetcode/easy/reverseLinkedList.js
+++ leetcode/easy/reverseLinkedList.js
@@ -23,5 +23,45 @@ const reverseLinkedList = (head) => {
     prev = curr;
     curr = nextNode;
   }
+  return prev;
 }
 
+/*
+let desc = 'short linked list';
+let nodes = valuesToLinkedListNodes([1, 2]);
+let reversedList = reverseLinkedList(nodes[0]);
+assertEquals(isListReversed(reversedList, nodes), true, desc);
+
+
+function valuesToLinkedListNodes(values) {
+  const nodes = [];
+  for (let i = 0; i < values.length; i++) {
+    const node = new LinkedListNode(values[i]);
+    if (i > 0) {
+      nodes[i - 1].next = node;
+    }
+    nodes.push(node);
+  }
+  return nodes;
+}
+
+function isListReversed(list, originalNodes) {
+  let i = originalNodes.length - 1;
+  while (list != null && i >= 0) {
+    if (originalNodes[i] != list) {
+      return false;
+    }
+    list = list.next;
+    i--;
+  }
+  return list == null;
+}
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
+*/
