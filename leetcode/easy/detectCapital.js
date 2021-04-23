eeace4d184222208cb293ad1ac4aaaab9e5b97e1
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-22 20:43:48 -0700

    detect capital

---
+++ leetcode/easy/detectCapital.js
@@ -0,0 +1,11 @@
+/**
+ * This question is asked by Google. Given a string, return whether or not it uses capitalization correctly.
+ * A string correctly uses capitalization if all letters are capitalized, no letters are capitalized,
+ * or only the first letter is capitalized.
+ * Ex: Given the following strings...
+ * "USA", return true
+ * "Calvin", return true
+ * "compUter", return false
+ * "coding", return true
+ */
+
