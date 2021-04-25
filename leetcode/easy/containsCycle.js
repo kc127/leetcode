912e72d6ca0cc4f4263f5bfc81e98411568f724e
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-24 23:27:36 -0700

    detect cycle

--- leetcode/easy/containsCycle.js
+++ leetcode/easy/containsCycle.js
@@ -9,4 +9,24 @@ class LinkedListNode {
   }
 }
 
+const detectCycle = (head) => {
+  if (head === null || head.next === null) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if(slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
 
