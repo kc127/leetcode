8aaaa65aa3054c244b64281bc87b1e6a5f4597d6
Kanchan Chauhan <kanchan827@gmail.com>
2021-05-09 21:36:12 -0700

    longest common prefix

---
+++ leetcode/easy/longestCommonPrefix.js
@@ -0,0 +1,10 @@
+/***
+ * Given an array of strings, return the longest common prefix
+ * that is shared amongst all strings.
+ * Note: you may assume all strings only contain lowercase
+ * alphabetical characters.
+ * Ex: Given the following arrays...
+ * ["colorado", "color", "cold"], return "col"
+ * ["a", "b", "c"], return ""
+ * ["spot", "spotty", "spotted"], return "spot"
+ */
