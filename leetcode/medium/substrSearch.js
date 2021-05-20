616a2826a6df018c9cc9a93a264d5faf2499fdf8
Kanchan Chauhan <kanchan827@gmail.com>
2021-05-20 11:16:33 -0700

    pattern matching - knuth moris pratt algorithm:

---
+++ leetcode/medium/substrSearch.js
@@ -0,0 +1,47 @@
+/* KMP algorithm for pattern matching */
+
+function strstr(s, x) {
+  let lps = computeTemporaryArray(x);
+
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < x.length) {
+    if (s[i] === x[j]) {
+      i++;
+      j++;
+    } else {
+      if (i !== 0) {
+        j = lps[j-1];
+      } else {
+        i++;
+      }
+    }
+  }
+  if (j === x.length) {
+    return true;
+  }
+  return false;
+}
+
+/* build a temporary array to maintain size of suffix which is same as prefix */
+const computeTemporaryArray = (x) => {
+  let lps = [];
+  lps[0] = 0;
+  let j = 0;
+  for (let i = 1; i < x.length;) {
+      if(x[i] === x[j]) {
+          lps[i] = j + 1;
+          i++;
+          j++;
+      } else {
+        if (j !== 0) {
+          j = lps[j-1];
+        } else {
+          lps[i] = 0;
+          i++;
+        }
+      }
+  }
+  return lps;
+}
+console.log(strstr('abc', 'abcdabca'))
