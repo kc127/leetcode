85844ead76cb33604891c2ce4695d75a5fba0344
Kanchan Chauhan <kanchan827@gmail.com>
2021-06-27 19:36:33 -0400

    time and space complexity

--- leetcode/easy/findElemSortedArrayWithDuplicates.js
+++ leetcode/easy/findElemSortedArrayWithDuplicates.js
@@ -1,5 +1,8 @@
 /***
  * Given a sorted array of integers and a target integer, find the first occurrence of the target and return its index. Return -1 if the target is not in the array
+ *
+ * time complexity: O(logn) because we divide the input by half at every step inside the while loop
+ * space complexity: O(1) because we use constant variables;
  */
 
 var findFirstOccurrence = (arr, target) => {
