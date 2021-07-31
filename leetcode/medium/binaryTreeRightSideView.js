7111045aec7f0a51019f731d52eaa48a5873f706
Kanchan Chauhan <kanchan827@gmail.com>
2021-07-31 19:05:27 -0400

    binary tree right side view

---
+++ leetcode/medium/binaryTreeRightSideView.js
@@ -0,0 +1,13 @@
+/**
+199. Binary Tree Right Side View
+
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+ */
+
+
+var rightSideView = function(root) => {
+  
+}
