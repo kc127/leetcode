259add44b2b4020b1d61c3372ba4ae5a5eb14990
Kanchan Chauhan <kanchan827@gmail.com>
2021-08-06 14:50:32 -0400

    palindromeLinkedList : two pointer with reverse second half in place technique

--- leetcode/easy/palindromeLinkedList.js
+++ leetcode/easy/palindromeLinkedList.js
@@ -6,6 +6,40 @@ Output: true
 Constraints: O(N) time and O(1) space
 */
 
+/*
+approach 2: Two Pointers with reverse linked list
+    get the end of the first half of the linked list by fast/slow pointer technique
+    get the second of the linked list (which is end of the first half . next)
+    reverse the second half
+    create two pointers p1 points to head
+                        p2 points to start of the second half
+    compare the values in nodes of first half with second half
+    reverse the second half
+    return result
+*/
+var isPalindromeOptimal = (head) => {
+  if (head === null) {
+    return true;
+  }
+
+  let firstHalfEnd = getFirstHalfEnd(head);
+  let secondHalfStart = reverseSecondHalf(firstHalfEnd.next);
+
+  let p1 = head;
+  let p2 = secondHalfStart;
+  let isPalindrome = true;
+  while (isPalindrome && p2 !== null) {
+    if (p1.val !== p2.val) {
+      return false;
+    }
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  firstHalfEnd.next = reverseSecondHalf(secondHalfStart);
+  return isPalindrome;
+}
+
 /* approach 1: brute force
    traverse through the linked list and store values in an array
    check if the array is a palindrome
