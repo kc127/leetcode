68916cd9a8d3a746b2015aaeeb901c417ba277d4
Kanchan Chauhan <kanchan827@gmail.com>
2021-08-15 09:33:36 -0400

    set matrix zeroes

---
+++ matrixProblems/diagonalTraverse.js
@@ -0,0 +1,9 @@
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+You must do it in place.
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+*/
