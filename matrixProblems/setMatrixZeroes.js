0ff4c43faa8ff3574ce5b150f41725751f473b61
Kanchan Chauhan <kanchan827@gmail.com>
2021-08-15 14:02:07 -0400

    set matrix zeroes: optimal solution

--- matrixProblems/setMatrixZeroes.js
+++ matrixProblems/setMatrixZeroes.js
@@ -52,6 +52,58 @@ var setZeroes = (matrix) => {
   return matrix;
 }
 
+
+/*
+Optimal Solution: O(1) Space Efficient Approach
+*/
+
+let setZeroesOptimal = (matrix) => {
+  let row = matrix.length;
+  let col = matrix[0].length;
+
+  let isCol = false;
+
+  // traverse the rows (0, row) and cols (1, cols)
+  for (let i = 0; i < row; i++) {
+    // if any cell in 0th col is 0, set isCol to true
+    if (matrix[i][0] === 0) {
+      isCol = true;
+    }
+    // traverse the cols excluding 0th col
+    for (let j = 1; j < col; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+  // traverse matrix again starting from 1st row, 1st col to set 0
+  for (let i = 1; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  // see if first cell is 0 and if first row needs to be set to 0
+  if (matrix[0][0] === 0) {
+    for (let j = 0; j < col; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  // see if first col needs to be set to be 0
+  if (isCol) {
+    for (let i = 0; i < row; i++) {
+      matrix[i][j] = 0;
+    }
+  }
+
+  return matrix;
+}
+
 let matrix = [[1,1,1],[1,0,1],[1,1,1]];
 
 console.log(setZeroes(matrix));
