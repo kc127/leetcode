6cf2530351ad84e6d2263450f153c8e6b9e5c98e
Kanchan Chauhan <kanchan827@gmail.com>
2021-08-19 23:44:52 -0400

    major minor diagonal

--- matrixProblems/majorMinorDiagonal.js
+++ matrixProblems/majorMinorDiagonal.js
@@ -1,17 +1,21 @@
 let printPrincipalDiagonal = (matrix, n) => {
   let major = [];
   let minor = [];
+  let majorSum = 1;
+  let minorSum = 1;
   for (let i = n - 1; i >= 0; i--) {
     for (let j = n - 1; j >= 0; j--) {
       if (i === j) {
         major.push(matrix[j][i]);
+        majorSum *= matrix[i][j];
       }
       if (i+j === n-1) {
         minor.push(matrix[j][i]);
+        minorSum *= matrix[i][j];
       }
     }
   }
-  return major;
+  return majorSum - minorSum;
 }
 
 
@@ -19,6 +23,8 @@ let matrix = [ [ 1, 2, 3, 4 ],
                [ 5, 6, 7, 8 ],
                [ 1, 2, 3, 4 ],
                [ 5, 6, 7, 8 ] ];
-let n = 4;
+let n = 2;
 
-console.log(printPrincipalDiagonal(matrix, n));
+let m = [[3,8],[4,6]]
+
+console.log(printPrincipalDiagonal(m, n));
