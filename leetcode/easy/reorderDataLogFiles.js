288455ce41b93dbd198928869d2f283c0ff2b620
Kanchan Chauhan <kanchan827@gmail.com>
2021-09-03 21:00:33 -0400

    reorder data of log files

---
+++ leetcode/easy/reorderDataLogFiles.js
@@ -0,0 +1,6 @@
+/*
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+
+*/
