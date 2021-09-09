c076e651f3dd0506450dcddd6a013e01ec27c6a1
Kanchan Chauhan <kanchan827@gmail.com>
2021-09-08 23:40:30 -0400

    binary search

---
+++ leetcode/easy/binarySearchII.js
@@ -0,0 +1,22 @@
+var binarySearch = (nums, target) => {
+  if (nums === null || nums.length === 0) {
+    return -1;
+  }
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left)/2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
