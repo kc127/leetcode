7437e0254de7001e0b8d56d60dc0f6b5c06cfd8a
Kanchan Chauhan <kanchan827@gmail.com>
2021-09-15 19:11:52 -0400

    second smallest

--- leetcode/easy/secondSmallest.js
+++ leetcode/easy/secondSmallest.js
@@ -1,19 +1,38 @@
+// var secondSmallest = (nums) => {
+//   let smallest = Number.MAX_SAFE_INTEGER;
+//   let secondSmallest = Number.MAX_SAFE_INTEGER;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === smallest) {
+//       secondSmallest = smallest;
+//     } else if (nums[i] < smallest) {
+//       secondSmallest = smallest;
+//       smallest = nums[i];
+//     } else if (nums[i] > smallest) {
+//       secondSmallest = nums[i];
+//     }
+//   }
+//   return [smallest, secondSmallest];
+// }
+
 var secondSmallest = (nums) => {
   let smallest = Number.MAX_SAFE_INTEGER;
   let secondSmallest = Number.MAX_SAFE_INTEGER;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === smallest) {
+  for (let num of nums) {
+    if (num === smallest) {
       secondSmallest = smallest;
-    } else if (nums[i] < smallest) {
+    } else if (num < smallest) {
       secondSmallest = smallest;
-      smallest = nums[i];
-    } else if (nums[i] > smallest) {
-      secondSmallest = nums[i];
+      smallest = num;
+    } else if (num > smallest) {
+      secondSmallest = num;
     }
   }
   return [smallest, secondSmallest];
 }
+
+
 
 let nums = [0 , 2 , 10 , 3, -3];
 console.log(secondSmallest(nums));
