dffc0daf65b61e0b447e86366c1bb59ab13196c2
Kanchan Chauhan <kanchan827@gmail.com>
2021-12-31 15:33:50 -0500

    path Sum

--- a-leetcode/easy/pathSum.js
+++ a-leetcode/easy/pathSum.js
@@ -7,3 +7,13 @@ Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
 Output: true
 
  */
+
+function hasPathSum (root, targetSum) {
+  if (!root) {
+    return false;
+  }
+  if (!root.left && !root.right && root.val === targetSum) {
+    return true;
+  }
+  return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+}
