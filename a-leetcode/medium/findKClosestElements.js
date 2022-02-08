0e5215b39d5c6eedcfc79b7cef228160ae58626f
Kanchan Chauhan <kanchan827@gmail.com>
2022-02-08 15:28:36 -0500

    find k closest elements

---
+++ a-leetcode/medium/findKClosestElements.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+ var findClosestElements = function(arr, k, x) {
+  let l = 0;
+  let r = arr.length - k;
+  while (l < r) {
+      let m = l + Math.floor((r-l)/2);
+      if (x - arr[m] > arr[m+k] - x) {
+          l = m + 1;
+      } else {
+          r = m;
+      }
+  }
+
+  return arr.slice(l, l+k);
+};
