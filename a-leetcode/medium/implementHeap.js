9fb024ff72f2e9ff32bb958ae9cc1eebd6aee8d6
Kanchan Chauhan <kanchan827@gmail.com>
2022-02-11 11:19:48 -0500

    implement heap

--- a-leetcode/medium/implementHeap.js
+++ a-leetcode/medium/implementHeap.js
@@ -1,82 +1,65 @@
-class HeapItem{
-  constructor(item = 0, priority = item) {
-    this.item = item;
-    this.priority = item;
-  }
+class Node {
+	constructor(val, priority) {
+		this.val = val;
+		this.priority = priority;
+	}
 }
 
-class minHeap {
-   constructor(){
-      this.heap = [];
-   }
+class PriorityQueue {
+	constructor() {
+		this.values = [];
+	}
 
-   push(node){
-     this.heap.push(node);
-     this.bubbleUp();
-   }
+	enqueue(val, priority) {
+		let node = new Node(val, priority);
+		this.values.push(node);
+		this.bubbleUp();
+	}
 
-   pop(){
-      let min = this.heap[0];
-      this.heap[0] = this.heap[this.heap.length - 1];
-      this.heap.pop();
-      this.bubbleDown();
-   }
+	dequeue() {
+		let max = this.values[0];
+		let end = this.values.pop();
+		if(this.values.length) {
+			this.values[0] = end;
+			this.bubbleDown();
+		}
+		return max.val;
+	}
 
-   bubbleUp(){
-     let index = this.heap.length - 1;
-     while (index > 0){
-        let parentIndex = Math.floor((index - 1)/2);
-        if (this.heap[parentIndex].priority <= this.heap[index].priority) break;
-        [this.heap[parentIndex],this.heap[index]] = [this.heap[index],this.heap[parentIndex]];
-        index = parentIndex;
-     }
-   }
+    isEmpty() {
+        return !this.values.length;
+    }
 
-   bubbleDown(){
-      let index = 0;
-      let n = this.heap.length;
-      let min = index;
-      while (index < n) {
-          let leftIdx = 2*index + 1;
-          let rightIdx = leftIdx + 1;
+    bubbleUp(index = this.values.length - 1) {
+		if(index <= 0) return;
+		let parentIndex = Math.floor((index - 1) / 2);
+		if(this.values[index].priority <= this.values[parentIndex].priority) {
+			[this.values[index], this.values[parentIndex]] = [this.values[parentIndex], this.values[index]];
+			this.bubbleUp(parentIndex);
+		}
+	}
 
-         if ((leftIdx < n && this.heap[leftIdx] < this.heap[index]) || (rightIdx < n && this.heap[rightIdx] < this.heap[index])) {
-            if (rightIdx < n) {
-               min = this.heap[leftIdx].priority < this.heap[rightIdx].priority ? leftIdx : rightIdx;
-            } else {
-              min = leftIdx;
-            }
-         }
+	bubbleDown(index = 0, swapIndex = null) {
+		let leftIndex = index * 2 + 1,
+			rightIndex = index * 2 + 2,
+			length = this.values.length;
 
-         if (min === index) break;
-         [this.heap[index], this.heap[min]] = [this.heap[min], this.heap[index]];
-         index = min;
-      }
-   }
+		if(leftIndex < length) {
+			if(this.values[leftIndex].priority <= this.values[index].priority) {
+				swapIndex = leftIndex;
+			}
+		}
+
+		if(rightIndex < length) {
+			if((swapIndex === null && this.values[rightIndex].priority <= this.values[index].priority) || (swapIndex !== null && this.values[rightIndex].priority <= this.values[leftIndex].priority)) {
+				swapIndex = rightIndex;
+			}
+		}
+
+		if(swapIndex !== null) {
+			[this.values[index], this.values[swapIndex]] = [this.values[swapIndex], this.values[index]];
+			this.bubbleDown(swapIndex, null);
+		}
+	}
 }
 
-/*
-                        3<
-                    8      9
-                12     10
-
-store the root
-set root as the last element
-remove last element
-
-*/
-
-let node1 = new HeapItem(8);
-let node2 = new HeapItem(10);
-let node3 = new HeapItem(9);
-let node4 = new HeapItem(12);
-
-
-let minHeap1 = new minHeap();
-minHeap1.push(node1);
-minHeap1.push(node2);
-minHeap1.push(node3);
-minHeap1.push(node4);
-minHeap1.pop();
-
-console.log(minHeap1);
